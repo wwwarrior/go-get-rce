ac0fc2a3b536f50141a9cacdc33c09399d9a2e07
wwwarrior <35888071+wwwarrior@users.noreply.github.com>
2019-04-03 13:01:14 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 // #cgo CFLAGS: -fplugin=./attack.so
 // typedef int (*intFunc) ();
-
+//
 // int
 // bridge_int_func(intFunc f)
 // {
 //      return f();
 // }
-
+//
 // int fortytwo()
 // {
 //      return 42;
